12dece95779883850aba9d09f906b096983fc57d
durgarao <asapudurgarao@gmail.com>
2018-07-16 08:39:43 +0530

    url changes in constants file

--- src/app/shared/others/constants.ts
+++ src/app/shared/others/constants.ts
@@ -5,7 +5,6 @@ export const REST_API_URL = 'http://139.59.6.52:8080/SpringMvcJdbcTemplate';
 /* ---------------     Node server ------------ */
 
 export const isLive = true;
-export const REST_API_URL_NODE = 'http://139.59.6.52:4000';
-
 // export const isLive = false;
-// export const REST_API_URL_NODE = 'http://localhost:4000';
+export const REST_API_URL_NODE = 'http://localhost:4000';
+// export const REST_API_URL_NODE = 'http://139.59.6.52:4000';
