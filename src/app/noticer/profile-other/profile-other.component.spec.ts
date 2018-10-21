d16ae72edb3c370e151986d478a612b1fbe6849f
durgarao <asapudurgarao@gmail.com>
2018-10-21 23:07:46 +0530

    issue fixed

--- src/app/noticer/profile-other/profile-other.component.spec.ts
+++ src/app/noticer/profile-other/profile-other.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { UserProfileComponent } from './profile-other.component';
+import { OthersProfileComponent } from './profile-other.component';
 
 
-describe('UserProfileComponent', () => {
-  let component: UserProfileComponent;
-  let fixture: ComponentFixture<UserProfileComponent>;
+describe('OthersProfileComponent', () => {
+  let component: OthersProfileComponent;
+  let fixture: ComponentFixture<OthersProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
+      declarations: [ OthersProfileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserProfileComponent);
+    fixture = TestBed.createComponent(OthersProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
