0f2ca6d9ab88fe888e8f06f9c1ae27076b56a294
durgarao <asapudurgarao@gmail.com>
2019-01-27 12:53:54 +0530

    help page build issues fixed

--- src/app/noticer/help/help.component.ts
+++ src/app/noticer/help/help.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MobileDetectionService } from '../../shared/services/mobiledetection.service';
 
 @Component({
   selector: 'help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css'],
-  providers: [],
+  providers: [MobileDetectionService],
   // animations: [routerTransition()]
 })
 export class HelpComponent implements OnInit {
 
-  constructor(private router: Router, private formbuilder: FormBuilder) { }
+  public isMobile: boolean;
+  constructor(private mobile: MobileDetectionService) { }
 
   ngOnInit() {
+    this.isMobile = this.mobile.isMobile();
   }
 
 }
